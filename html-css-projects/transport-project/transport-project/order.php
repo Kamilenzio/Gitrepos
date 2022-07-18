0fc75586c85b3be87d4dc951ce5fa73f1002d171
Kamilenzio <kamil.jagielnik95@gmail.com>
2022-07-18 21:51:17 +0200

    segregation

---
+++ html-css-projects/transport-project/transport-project/order.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<title>Piekarnia</title>
+</head>
+<body>
+
+<?php
+echo<<<END
+
+<h1>Podsumowanie zamowienia</h1><br>
+<table border="1" cellpadding="10" cellspacing="0">
+<tr>
+<td>
+Twoje zamowienie to: 
+<br>
+<a href="index.html">Powrót do strony głównej</a>
+
+	</br>
+END;
+?>
+
+
+
+</body>
+</html>
+
