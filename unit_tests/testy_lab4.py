7cfab99228f5a1270802305398f0b53e3660f59c
Kamilenzio <kamil.jagielnik95@gmail.com>
2023-01-28 15:05:02 +0100

    unit tests rework

--- unit_tests/testy_lab4.py
+++ unit_tests/testy_lab4.py
@@ -42,19 +42,39 @@ def test_no_real_elements(capsys): #capture system - pobiera eventy systemowe, m
     #then
     assert result
 
-def get_2_real_element(a: int,b: int, c: int) -> int:
-    delta = b**2 - 4*a*c
-    return delta > 0
 
-def test_2_real_element(capsys): #capture system - pobiera eventy systemowe, mozna dzieki temu pobierac to co zwraca program
+def get_1_real_element(a: int,b: int, c: int) -> int:
+    delta = b**2 - 4*a*c
+    return delta == 0
+
+def test_1_real_element(capsys): #capture system - pobiera eventy systemowe, mozna dzieki temu pobierac to co zwraca program
     # try
-    a = 2
-    b = 5
-    c = 3
+    a = 1
+    b = -2
+    c = 1
 
     #when
-    result = get_2_real_element(a, b, c)
+    result = get_1_real_element(a, b, c)
     
     #then
     assert result
+
+
+def get_2_real_elements(a: int,b: int, c: int) -> int:
+    delta = b**2 - 4*a*c
+    return delta > 0
+
+def test_2_real_elements(capsys): #capture system - pobiera eventy systemowe, mozna dzieki temu pobierac to co zwraca program
+    # try
+    a = 1
+    b = 0
+    c = -3
+
+    #when
+    result = get_2_real_elements(a, b, c)
+    
+    #then
+    assert result
+
+
     
